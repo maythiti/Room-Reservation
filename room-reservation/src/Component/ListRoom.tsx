53838bfded13dbf0110c4e851f8d003074826d23
thitiratt <pinterest.mttr@gmail.com>
2020-02-20 10:00:54 +0700

    add icon

--- room-reservation/src/Component/ListRoom.tsx
+++ room-reservation/src/Component/ListRoom.tsx
@@ -1,15 +1,42 @@
 import React, { FC, useState, useEffect, Suspense } from "react";
-import EditModal from './EditModal'
-import DeleteModal from './DeleteModal'
-import AddModal from './AddModal'
+import EditModal from "./EditModal";
+import DeleteModal from "./DeleteModal";
+import AddModal from "./AddModal";
+import { createStyles, Fab } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme =>
+  createStyles({
+    mainButton: {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+      position: "fixed"
+    }
+  })
+);
 const ListRoom: FC = () => {
-  return (<div>
-    Hello React
-    <EditModal />
-    <DeleteModal />
-    <AddModal />
-  </div>)
+  const classes = useStyles()
+
+  function onOpenCreate() {
+    console.log('add icon was clicked')
+  }
+  return (
+    <div>
+      Hello React
+      <EditModal />
+      <DeleteModal />
+      <AddModal />
+      <Fab
+        className={classes.mainButton}
+        color="primary"
+        aria-label="add"
+        onClick={onOpenCreate}
+      >
+        <AddIcon />
+      </Fab>
+    </div>
+  );
 };
 
 export default ListRoom;
